232e90e82976e5e4c89ea1687513db8bb494a7fe
ADINESHKARTHICK <dinesha1406@gmail.com>
2024-08-20 15:57:06 +0530

    Concatenation.java

---
+++ concatenation.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Days
+{
+    public static void main(String[] args)
+    { 
+        Scanner s=new Scanner(System.in);
+        String Fname,Lname;
+        System.out.println("");
+        Fname=s.next();
+        System.out.println("");
+        Lname=s.next();
+        System.out.println(Fname.concat(Lname));
+    }
+}
