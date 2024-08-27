1e3920d32062e687fff41c1ed57cf40e04402585
ADINESHKARTHICK <dinesha1406@gmail.com>
2024-08-27 14:50:40 +0530

    swapnumber.java

---
+++ swapnumber.java
@@ -0,0 +1,14 @@
+class Number {
+    public static void main(String[] args) {
+       int n=123456, rev=0,s=0;
+       for(int i=n;i>0;i=i/10){
+           int r=i%10;
+           rev=rev*10+r;
+       }
+       for(int i=rev;i>0;i=i/100){
+           int r=i%100;
+           s=s*100+r;
+       }
+       System.out.print(s);
+    }
+}
