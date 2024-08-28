ff18d1f5489c07147650486dc5c8daeb7a5cca7d
ADINESHKARTHICK <dinesha1406@gmail.com>
2024-08-28 15:29:22 +0530

    Create  Asciivalues.java

---
+++ Asciivalues.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Asciivalues{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        for(int i=0;i<a.length();i++){
+         int ch=a.charAt(i);
+        System.out.println(ch);
+    }
+}
+}
