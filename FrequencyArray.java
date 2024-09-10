cf79ee1f77de7b0132ac529487a688d8c1f2f884
ADINESHKARTHICK <dinesha1406@gmail.com>
2024-09-10 09:56:55 +0530

    Create FrequencyArray.java

---
+++ FrequencyArray.java
@@ -0,0 +1,14 @@
+ import java.util.*;
+ public class FrequencyArray{
+     public static void main(String[] args){
+         int madhan[]={1,1,1,2,2,3,4,4,4};
+         int fmaadhava[]=new int[257];
+         for(int i=0;i<9;i++){
+             fmaadhava[madhan[i]]++;
+         }
+         for(int i=0;i<257;i++){
+             if(fmaadhava[i]>0)
+              System.out.print(i+" "+fmaadhava[i]);
+         }
+     }
+ }
